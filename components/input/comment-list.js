5cc642130518498f905e36165fb2299a6e73569c
Sheila Kobue <maserole.mojela@gmail.com>
2023-09-20 11:22:53 +0200

    moved file

---
+++ components/input/comment-list.js
@@ -0,0 +1,20 @@
+import commentStyles from "./comment-list.module.css";
+
+function CommentList(props) {
+  const { items } = props;
+
+  return (
+    <ul className={commentStyles.comments}>
+      {items.map((item) => (
+        <li key={item._id}>
+          <p>{item.text}</p>
+          <div>
+            By <address>{item.name}</address>
+          </div>
+        </li>
+      ))}
+    </ul>
+  );
+}
+
+export default CommentList;
